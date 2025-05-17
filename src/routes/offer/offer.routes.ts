507c4cf748720743a32262cd92af8a8732ab340a
luizanurnberg <nurnbergluiza@gmail.com>
2025-05-17 17:05:16 -0300

    feat(middleware): add middleware to validate token

--- src/routes/offer/offer.routes.ts
+++ src/routes/offer/offer.routes.ts
@@ -1,20 +1,20 @@
 import { Router } from "express";
 import OfferController from "../../controllers/offer/offer.controller";
-import { checkAuth } from "../../middleware/checkAuth";
+import { authMiddleware } from "../../middleware/auth.middleware";
 
 const offerRoutes = Router();
 
 // Rotas POST
-offerRoutes.post("/offer", checkAuth, OfferController.create);
-offerRoutes.post("/approveOffer", checkAuth, OfferController.approveOffer);
+offerRoutes.post("/offer", authMiddleware, OfferController.create);
+offerRoutes.post("/offer/approve", authMiddleware, OfferController.approveOffer);
 
 // Rotas GET
-offerRoutes.get("/offer", checkAuth, OfferController.findAll);
-offerRoutes.get("/offer/:id", checkAuth, OfferController.findOne);
-offerRoutes.get("/offerByQuotation/:id", checkAuth, OfferController.findOfferByQuotationId);
+offerRoutes.get("/offer", authMiddleware, OfferController.findAll);
+offerRoutes.get("/offer/:id", authMiddleware, OfferController.findOne);
+offerRoutes.get("/offer/quotation/:id", authMiddleware, OfferController.findOfferByQuotationId);
 
 // Rotas DELETE e PUT
-offerRoutes.delete("/offer/:id", checkAuth, OfferController.delete);
-offerRoutes.put("/offer", checkAuth, OfferController.update);
+offerRoutes.delete("/offer/:id", authMiddleware, OfferController.delete);
+offerRoutes.put("/offer", authMiddleware, OfferController.update);
 
 export default offerRoutes;
--- src/routes/quotation/quotation.routes.ts
+++ src/routes/quotation/quotation.routes.ts
@@ -1,20 +1,20 @@
 import { Router } from 'express';
 import QuotationController from "../../controllers/quotation/quotation.controller";
-import { checkAuth } from '../../middleware/checkAuth';
+import { authMiddleware } from '../../middleware/auth.middleware';
 
 const quotationRoutes = Router();
 
 // Rotas POST
-quotationRoutes.post("/quotation-with-addresses", checkAuth, QuotationController.createWithAddresses);
+quotationRoutes.post("/quotation-with-addresses", authMiddleware, QuotationController.createWithAddresses);
 
 // Rotas GET
-quotationRoutes.get("/quotation", checkAuth, QuotationController.findAll);
-quotationRoutes.get("/quotation/:id", checkAuth, QuotationController.findOne);
-quotationRoutes.get("/quotation-by-cpf/:cpf", checkAuth, QuotationController.findAllByCPF);
-quotationRoutes.get("/quotation-no-offers", checkAuth, QuotationController.findAllWithoutApprovedOffers);
+quotationRoutes.get("/quotation", authMiddleware, QuotationController.findAll);
+quotationRoutes.get("/quotation/:id", authMiddleware, QuotationController.findOne);
+quotationRoutes.get("/quotation-by-cpf/:cpf", authMiddleware, QuotationController.findAllByCPF);
+quotationRoutes.get("/quotation-no-offers", authMiddleware, QuotationController.findAllWithoutApprovedOffers);
 
 // // Rotas PUT e DELETE
-quotationRoutes.put("/quotation/:id", checkAuth, QuotationController.update);
-quotationRoutes.delete("/quotation/:id", checkAuth, QuotationController.deleteOne);
+quotationRoutes.put("/quotation/:id", authMiddleware, QuotationController.update);
+quotationRoutes.delete("/quotation/:id", authMiddleware, QuotationController.deleteOne);
 
 export default quotationRoutes;
